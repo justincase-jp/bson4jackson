59e15fbd1b4625857de11543bd47e86743ba5884
Nicholas Yip <nicholas.yip@justincase-tech.com>
2019-10-15 14:59:23 +0900

    Update `BsonUuidSerializer` to use big endian encoding

--- src/main/java/de/undercouch/bson4jackson/serializers/BsonUuidSerializer.java
+++ src/main/java/de/undercouch/bson4jackson/serializers/BsonUuidSerializer.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.UUIDSerializer;
 import de.undercouch.bson4jackson.BsonConstants;
 import de.undercouch.bson4jackson.BsonGenerator;
+import org.bson.BsonBinary;
+import org.bson.UuidRepresentation;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -22,29 +24,18 @@ public class BsonUuidSerializer extends JsonSerializer<UUID> {
         if (gen instanceof BsonGenerator) {
             BsonGenerator bgen = (BsonGenerator)gen;
             bgen.writeBinary(null, BsonConstants.SUBTYPE_UUID,
-                    uuidToLittleEndianBytes(value), 0, 16);
+                    uuidToBigEndianBytes(value), 0, 16);
         } else {
             new UUIDSerializer().serialize(value, gen, provider);
         }
     }
 
     /**
-     * Utility routine for converting UUIDs to bytes in little endian format.
+     * Utility routine for converting UUIDs to bytes in big endian format.
      * @param uuid The UUID to convert
-     * @return a byte array representing the UUID in little endian format
+     * @return a byte array representing the UUID in big endian format
      */
-    protected static byte[] uuidToLittleEndianBytes(UUID uuid) {
-        long msb = uuid.getMostSignificantBits();
-        long lsb = uuid.getLeastSignificantBits();
-        byte[] buffer = new byte[16];
-
-        for (int i = 0; i < 8; i++) {
-            buffer[i] = (byte) (msb >>> 8 * i);
-        }
-        for (int i = 8; i < 16; i++) {
-            buffer[i] = (byte) (lsb >>> 8 * (i - 16));
-        }
-
-        return buffer;
+    protected static byte[] uuidToBigEndianBytes(UUID uuid) {
+        return new BsonBinary(uuid, UuidRepresentation.STANDARD).getData();
     }
 }
